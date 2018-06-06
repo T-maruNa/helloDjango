5355cb48d3b714ab9519c055e6dbf85b3d4733f1
T-maruNa <tomaru.naoki@gmail.com>
2018-06-07 00:20:33 +0900

    /hello /helloTest にて画面表示追加

--- helloDjango/urls.py
+++ helloDjango/urls.py
@@ -13,10 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.contrib import admin
-from django.urls import include,path
+from hello.views import home,test
 
 urlpatterns = [
-    path('hello/', include(hello.urls)),
-    path('admin/', admin.site.urls),
+    url('hello/', home, name='home'),
+    url('helloTest/', test, name='test'),
+    url('admin/', admin.site.urls),
 ]
