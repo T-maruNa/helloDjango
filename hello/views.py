08850563d60ddce980055b6b4601e9a4a59c4d1a
T-maruNa <tomaru.naoki@gmail.com>
2018-06-18 21:11:11 +0900

    Django テンプレート呼び出し

--- hello/views.py
+++ hello/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Hello, Django World")
+    return render(request, 'hello.html')
 
 def test(request):
     return HttpResponse("Test, Django World")
